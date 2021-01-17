46fd91539594616273ae6704c99bdd13cb0d0667
Mia Kaupaite <emilijakpt@gmail.com>
2021-01-17 09:56:56 -0500

    initial set-up of project, hopefully it's not horrible

---
+++ CarCompanion/app/src/main/java/edu/rosehulman/kaupaies/carcompanion/ui/login/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package edu.rosehulman.kaupaies.carcompanion.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.rosehulman.kaupaies.carcompanion.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
