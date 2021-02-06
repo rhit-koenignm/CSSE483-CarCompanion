17fe4331f91d4d2c0e7d6263ea5b8768118292f6
rhit-koenignm <70714241+rhit-koenignm@users.noreply.github.com>
2021-02-06 18:14:01 -0500

    updated the trouble adapter, will come back to it

--- CarCompanion/app/src/main/java/edu/rosehulman/kaupaies/carcompanion/ui/troubleshooting/TroubleAdapter.kt
+++ CarCompanion/app/src/main/java/edu/rosehulman/kaupaies/carcompanion/ui/troubleshooting/TroubleAdapter.kt
@@ -4,4 +4,6 @@ import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 
 class TroubleAdapter(var context: Context?, var listener: TroubleshootingFragment.OnTroubleSelectedListener?) : RecyclerView.Adapter<TroubleViewHolder> {
+
+
 }
